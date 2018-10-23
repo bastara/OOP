6ba5c59c49d7aace1e5dc6116aa3086f2add0373
bastara <kolobokuu@yandex.ru>
2018-10-23 12:10:00 +0700

    сделал вектор, осталось exceptions

--- Vector/src/ru/academit/kolobov/vector/Main.java
+++ Vector/src/ru/academit/kolobov/vector/Main.java
@@ -9,24 +9,60 @@ public class Main {
         double[] c2 = {3, -7, 2, -4};
         Vector v2 = new Vector(c2);
         Vector v3 = new Vector(v1);
-
-        System.out.println(v1.getSize());
-
-        System.out.println(v2.getSize());
-
-        System.out.println(v3.getSize());
-        for (double v : v3.getArray()) {
-            System.out.println(v);
-        }
-
+        double[] c5 = {1, -4, 1, 1};
         Vector v4 = new Vector(5, c1);
+        Vector v5 = new Vector(c5);
 
-        System.out.println(v4.getSize());
-        for (double v : v4.getArray()) {
-            System.out.println(v);
-        }
 
         System.out.println("Размер вектора v1: " + v1.getSize());
         System.out.println("Размер вектора v5: " + v4.getSize());
+
+        System.out.println("Координаты вектора v3:" + v3.toString());
+        System.out.println("Координаты вектора v4:" + v4.toString());
+
+        Vector v6 = v1.addVector(v5);
+        System.out.println("Координаты вектора v6(сложение v1 и v5):" + v6.toString());
+
+        Vector v7 = v4.substractinonVector(v6);
+        System.out.println("Координаты вектора v7(вычитание из v4 вектора v6): " + v7.toString());
+
+        v7.invertVector();
+        System.out.println("Разворот вектора v7 и его новые координаты: " + v7.toString());
+
+        double[] c8 = {1, 1, 9};
+        Vector v8 = new Vector(c8);
+        System.out.println("Длина вектора v8: " + v8.lengthVector());
+
+        System.out.println("3 элемент вектора v5 равен: " + v5.getElementVector(3));
+
+        v5.setElementVector(3, 8);
+        System.out.println("3 элемент вектора v5 равен: " + v5.getElementVector(3));
+
+        System.out.println("Сравниваем v2 и v5:");
+        if (v2.equals(v5)) {
+            System.out.println("Векторы равны");
+        } else {
+            System.out.println("Векторы не равны");
+        }
+
+        double[] c9 = {3, -7, 2, -4};
+        Vector v9 = new Vector(c9);
+
+        System.out.println("Сравниваем v2 и v9:");
+        if (v2.equals(v9)) {
+            System.out.println("Векторы равны");
+        } else {
+            System.out.println("Векторы не равны");
+        }
+
+        System.out.println("Сложение векторов v4 и v6:");
+        Vector v10 = Vector.sum(v4, v6);
+        System.out.println("Координаты вектора v10:" + v10.toString());
+
+        System.out.println("Вычитание из v4 вектора v6:");
+        Vector v11 = Vector.dif(v4, v6);
+        System.out.println("Координаты вектора v11:" + v11.toString());
+
+        System.out.println("Скалярное произведение векторов v5 и v8 будет: " + Vector.scalarVector(v5, v8));
     }
 }
--- Vector/src/ru/academit/kolobov/vector/Vector/Vector.java
+++ Vector/src/ru/academit/kolobov/vector/Vector/Vector.java
@@ -15,45 +15,161 @@ public class Vector {
     public Vector(Vector vector) {
         dimension = vector.dimension;
         array = new double[vector.array.length];
-        for (int i = 0; i < dimension; i++) {
-            array[i] = vector.array[i];
-        }
+        if (dimension >= 0) System.arraycopy(vector.array, 0, array, 0, dimension);
     }
 
     public Vector(double[] a) {
         array = new double[a.length];
         dimension = a.length;
-        for (int i = 0; i < dimension; i++) {
-            array[i] = a[i];
-        }
+        if (dimension >= 0) System.arraycopy(a, 0, array, 0, dimension);
     }
-
-//    public Vector(double... x) {// конструктор на случай заполнения не из массива а координатами
-//        array = new double[x.length];
-//        dimension = x.length;
-//        for (int i = 0; i < dimension; i++) {
-//            array[i] = x[i];
-//        }
-//    }
 
     public Vector(int n, double[] a) {
         array = new double[n];
         dimension = n;
-        for (int i = 0; i < a.length; i++) {
-            array[i] = a[i];
-        }
+        if (a.length >= 0) System.arraycopy(a, 0, array, 0, a.length);
         for (int i = a.length; i < n; i++) {
             array[i] = 0;
         }
     }
 
-
     public int getSize() {
         return dimension;
     }
 
-    public double[] getArray() {
-        return array;
+    @Override
+    public String toString() {
+        StringBuilder str = new StringBuilder("{");
+        for (int i = 0; i < array.length - 1; i++) {
+            str.append(array[i]).append(", ");
+        }
+        return str.toString() + array[array.length - 1] + "}";
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 37;
+        int hash = 1;
+        hash = prime * hash + dimension;
+        for (double c : array) {
+            hash = prime * hash + Double.hashCode(c);
+        }
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+
+        if (o == null || this.getClass() != o.getClass()) {
+            return false;
+        }
+
+        Vector p = (Vector) o;
+        for (int i = 0; i < p.array.length; i++) {
+            if (array[i] != p.array[i]) {
+                return false;
+            }
+        }
+        return dimension == p.dimension;
+    }
+
+
+    public Vector addVector(Vector v2) {
+        boolean b = this.array.length > v2.array.length;
+        int nMax = Math.max(this.array.length, v2.array.length);
+        int nMin = Math.min(this.array.length, v2.array.length);
+        double[] arr = new double[nMax];
+        for (int i = 0; i < nMin; i++) {
+            arr[i] = this.array[i] + v2.array[i];
+        }
+        if (b) {
+            if (nMax - nMin >= 0) System.arraycopy(this.array, nMin, arr, nMin, nMax - nMin);
+        } else {
+            if (nMax - nMin >= 0) System.arraycopy(v2.array, nMin, arr, nMin, nMax - nMin);
+        }
+        return new Vector(nMax, arr);
+    }
+
+    public static Vector sum(Vector v1, Vector v2) {
+        boolean b = v1.array.length > v2.array.length;
+        int nMax = Math.max(v1.array.length, v2.array.length);
+        int nMin = Math.min(v1.array.length, v2.array.length);
+        double[] arr = new double[nMax];
+        for (int i = 0; i < nMin; i++) {
+            arr[i] = v1.array[i] + v2.array[i];
+        }
+        if (b) {
+            System.arraycopy(v1.array, nMin, arr, nMin, nMax - nMin);
+        } else {
+            System.arraycopy(v2.array, nMin, arr, nMin, nMax - nMin);
+        }
+        return new Vector(nMax, arr);
+    }
+
+    public static Vector dif(Vector v1, Vector v2) {
+        boolean b = v1.array.length > v2.array.length;
+        int nMax = Math.max(v1.array.length, v2.array.length);
+        int nMin = Math.min(v1.array.length, v2.array.length);
+        double[] arr = new double[nMax];
+        for (int i = 0; i < nMin; i++) {
+            arr[i] = v1.array[i] - v2.array[i];
+        }
+        if (b) {
+            System.arraycopy(v1.array, nMin, arr, nMin, nMax - nMin);
+        } else {
+            System.arraycopy(v2.array, nMin, arr, nMin, nMax - nMin);
+        }
+        return new Vector(nMax, arr);
+    }
+
+    public Vector substractinonVector(Vector v2) {
+        boolean b = this.array.length > v2.array.length;
+        int nMax = Math.max(this.array.length, v2.array.length);
+        int nMin = Math.min(this.array.length, v2.array.length);
+        double[] arr = new double[nMax];
+        for (int i = 0; i < nMin; i++) {
+            arr[i] = this.array[i] - v2.array[i];
+        }
+        if (b) {
+            if (nMax - nMin >= 0) System.arraycopy(this.array, nMin, arr, nMin, nMax - nMin);
+        } else {
+            if (nMax - nMin >= 0) System.arraycopy(v2.array, nMin, arr, nMin, nMax - nMin);
+        }
+        return new Vector(nMax, arr);
+    }
+
+    public void invertVector() {
+        for (int i = 0; i < this.array.length; i++) {
+            array[i] = (this.array[i] == 0) ? 0 : this.array[i] * (-1);
+        }
+    }
+
+    public double lengthVector() {
+        double sum = 0;
+        for (double c : this.array) {
+            sum += c * c;
+        }
+        return Math.sqrt(sum);
+    }
+
+    public double getElementVector(int e) {
+        return this.array[e];
+    }
+
+    public void setElementVector(int e, double c) {
+        this.array[e] = c;
+    }
+
+    public static double scalarVector(Vector v1, Vector v2) {
+        int nMin = Math.min(v1.array.length, v2.array.length);
+        double scalar = 0;
+        for (int i = 0; i < nMin; i++) {
+            scalar += v1.array[i] * v2.array[i];
+        }
+        return scalar;
     }
 }
 
