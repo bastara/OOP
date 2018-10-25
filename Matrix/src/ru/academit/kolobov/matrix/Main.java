b4414db494a0964078b44b1ff7eaa27d6dac38ae
bastara <kolobokuu@yandex.ru>
2018-10-25 18:19:56 +0700

    matrix сделал с выводом вектора по столбцу

--- Matrix/src/ru/academit/kolobov/matrix/Main.java
+++ Matrix/src/ru/academit/kolobov/matrix/Main.java
@@ -22,5 +22,29 @@ public class Main {
         Matrix m1 = new Matrix(m);
         System.out.println(m1.getArray()[0][2]);
         System.out.println(m1.getArray()[2][3]);
+
+        //получаем размер матрицы m1
+        System.out.println("Размер матрицы m1: " + m1.getSizeY(m1) + "-" + m1.getSizeX(m1));
+
+        //Получение и задание вектора-строки по индексу
+        Vector v4 = m1.getVector(2);
+        System.out.println("Получение вектора по индексу 2: " + m1.getVector(2));
+        //Вывод вектора методом toString из класса Vector
+        System.out.println("Координаты вектора по индексу 2: " + v4.toString());
+
+
+        //toString определить так, чтобы результат получался в таком виде: { { 1, 2 }, { 2, 3 } }
+        System.out.println("Вывод матрицы переопределенным методом toString:");
+        System.out.println(m1.toString());
+
+        //Транспонирование матрицы
+        Matrix m2 = m1.getTranspositionMatrix();
+        System.out.println("Вывод транспонированной матрицы:");
+        System.out.println(m2.toString());
+        System.out.println();
+
+        //Получение вектора-столбца по индексу
+        Vector v5 = m1.getVectorColumn(2);
+        System.out.println("Получение вектора по столбцу 2: " + v5.toString());
     }
 }
--- Matrix/src/ru/academit/kolobov/matrix/matrix/Matrix.java
+++ Matrix/src/ru/academit/kolobov/matrix/matrix/Matrix.java
@@ -41,15 +41,61 @@ public class Matrix {
         }
     }
 
-    public Matrix(double [][] array) {
-        int y = array.length;
-        int x = array[0].length;
+    public Matrix(double[][] a) {
+        int y = a.length;
+        int x = a[0].length;
         array = new double[y][x];
         for (int i = 0; i < y; i++) {
             for (int j = 0; j < x; j++) {
-                array[i][j] = array[i][j];
+                array[i][j] = a[i][j];
             }
         }
     }
 
+    public int getSizeX(Matrix matrix) {
+        return matrix.getArray()[0].length;
+    }
+
+    public int getSizeY(Matrix matrix) {
+        return matrix.getArray().length;
+    }
+
+    public Vector getVector(int i) {
+        return new Vector(this.getArray()[i]);
+    }
+
+    @Override
+    public String toString() {
+        String str = "{";
+        for (int i = 0; i < array.length; i++) {
+            if (i > 0) {
+                str += ",";
+            }
+            str += "{";
+            for (int j = 0; j < array[0].length - 1; j++) {
+                str += array[i][j] + ", ";
+            }
+            str += array[i][array[0].length - 1] + "}";
+        }
+        str += "}";
+        return str;
+    }
+
+    public Matrix getTranspositionMatrix() {
+        int y = array.length;
+        int x = array[0].length;
+        double[][] arrayTrp = new double[x][y];
+        for (int i = 0; i < x; i++) {
+            for (int j = 0; j < y; j++) {
+                arrayTrp[i][j] = array[j][i];
+            }
+        }
+        return new Matrix(arrayTrp);
+    }
+
+    public Vector getVectorColumn(int i) {
+        Matrix m = getTranspositionMatrix();
+        return m.getVector(i);
+    }
 }
+
