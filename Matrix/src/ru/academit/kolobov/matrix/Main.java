c8b058db942c2aaa36e714f62c6a18934f46916c
bastara <kolobokuu@yandex.ru>
2018-10-27 21:57:28 +0700

    matrix делаю определитель

--- Matrix/src/ru/academit/kolobov/matrix/Main.java
+++ Matrix/src/ru/academit/kolobov/matrix/Main.java
@@ -5,23 +5,46 @@ import ru.academit.kolobov.vector.Vector.Vector;
 
 public class Main {
     public static void main(String[] args) {
-        double[] c1 = {2, 5, 6, 3};
+//        double[] c1 = {2, 5, 6, 3};
+//        Vector v1 = new Vector(c1);
+//        double[] c2 = {4, -7, -8, 4};
+//        Vector v2 = new Vector(c2);
+//        double[] c3 = {-6, 12, 3, 5};
+//        Vector v3 = new Vector(c3);
+
+//        double[] c1 = {0, 5, 6, 3, 23};
+//        Vector v1 = new Vector(c1);
+//        double[] c2 = {4, -7, -8, 4, 1};
+//        Vector v2 = new Vector(c2);
+//        double[] c3 = {-6, 12, 3, 5, -4};
+//        Vector v3 = new Vector(c3);
+//        double[] c4 = {8, 5, -4, - 4, 0};
+//        Vector v6 = new Vector(c4);
+//        double[] c5 = {3, 3, 0, 19, -8};
+//        Vector v7 = new Vector(c5);
+
+        double[] c1 = {0, 3, -1, 2, 6};
         Vector v1 = new Vector(c1);
-
-        double[] c2 = {4, -7, -8, 4};
+        double[] c2 = {2, 1, 0, 0, 3};
         Vector v2 = new Vector(c2);
-
-        double[] c3 = {-6, 12, 3, 5};
+        double[] c3 = {-2, -1, 0, 2, 5};
         Vector v3 = new Vector(c3);
+        double[] c4 = {-5, 7, 1, 1, 1};
+        Vector v6 = new Vector(c4);
+        double[] c5 = {2, 0, 2, -2, 1};
+        Vector v7 = new Vector(c5);
 
-        Vector[] arrayV = {v1, v2, v3};
+        Vector[] arrayV = {v1, v2, v3, v6, v7};
 
         Matrix m = new Matrix(arrayV);
 
         //копируем матрицу m
         Matrix m1 = new Matrix(m);
-        System.out.println(m1.getArray()[0][2]);
-        System.out.println(m1.getArray()[2][3]);
+
+        //toString определить так, чтобы результат получался в таком виде: { { 1, 2 }, { 2, 3 } }
+        System.out.println("Вывод матрицы m1 переопределенным методом toString:");
+        System.out.println(m1);
+        System.out.println();
 
         //получаем размер матрицы m1
         System.out.println("Размер матрицы m1: " + m1.getSizeY(m1) + "-" + m1.getSizeX(m1));
@@ -30,21 +53,28 @@ public class Main {
         Vector v4 = m1.getVector(2);
         System.out.println("Получение вектора по индексу 2: " + m1.getVector(2));
         //Вывод вектора методом toString из класса Vector
-        System.out.println("Координаты вектора по индексу 2: " + v4.toString());
-
-
-        //toString определить так, чтобы результат получался в таком виде: { { 1, 2 }, { 2, 3 } }
-        System.out.println("Вывод матрицы переопределенным методом toString:");
-        System.out.println(m1.toString());
-
-        //Транспонирование матрицы
-        Matrix m2 = m1.getTranspositionMatrix();
-        System.out.println("Вывод транспонированной матрицы:");
-        System.out.println(m2.toString());
+        System.out.println("Координаты вектора по индексу 2(через класс веторов): " + v4);
         System.out.println();
 
         //Получение вектора-столбца по индексу
         Vector v5 = m1.getVectorColumn(2);
-        System.out.println("Получение вектора по столбцу 2: " + v5.toString());
+        System.out.println("Получение вектора по столбцу 2: " + v5);
+        System.out.println();
+
+        //Транспонирование матрицы
+        Matrix m2 = m1.getTransposition();
+        System.out.println("Вывод транспонированной матрицы m2:");
+        System.out.println(m2);
+        System.out.println();
+
+
+        //Умножение на скаляр
+        System.out.println("m1: " + m1);
+        m1.multiplicationByScalar(-4);
+        System.out.println("m1 умноженная на скаляр (-4): " + m1);
+        System.out.println();
+
+        //Вычисление определителя матрицы
+        int determinamt = m.determinant();
     }
 }
--- Matrix/src/ru/academit/kolobov/matrix/matrix/Matrix.java
+++ Matrix/src/ru/academit/kolobov/matrix/matrix/Matrix.java
@@ -6,33 +6,27 @@ public class Matrix {
     private double[][] array;
 
     public Matrix(Vector[] vectors) {
-        //помню что у меня была еще переменная dimension, тут я ее упускаю. Может в векторе ее тоже убрать и размер брать от длинны? Или оставить и разместить например в [0] индексе?
         int y = vectors.length;
-        int x = vectors[0].getArray().length;
+        int x = vectors[0].getSize();
         array = new double[y][x];
         for (int i = 0; i < y; i++) {
             for (int j = 0; j < x; j++) {
-                array[i][j] = vectors[i].getArray()[j];
+                array[i][j] = vectors[i].getElement(j);
             }
         }
     }
 
     public Matrix(int n, int m) {
         array = new double[n][m];
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < m; j++) {
-                array[i][j] = 0;
-            }
-        }
     }
 
     public Matrix(Matrix matrix) {
-        int y = matrix.getArray().length;
-        int x = matrix.getArray()[0].length;
+        int y = matrix.array.length;
+        int x = matrix.array[0].length;
         array = new double[y][x];
         for (int i = 0; i < y; i++) {
             for (int j = 0; j < x; j++) {
-                array[i][j] = matrix.getArray()[i][j];
+                array[i][j] = matrix.array[i][j];
             }
         }
     }
@@ -48,40 +42,36 @@ public class Matrix {
         }
     }
 
-    public double[][] getArray() {
-        return array;
-    }
-
     public int getSizeX(Matrix matrix) {
-        return matrix.getArray()[0].length;
+        return matrix.array[0].length;
     }
 
     public int getSizeY(Matrix matrix) {
-        return matrix.getArray().length;
+        return matrix.array.length;
     }
 
     public Vector getVector(int i) {
-        return new Vector(this.getArray()[i]);
+        return new Vector(array[i]);
     }
 
     @Override
     public String toString() {
-        String str = "{";
+        StringBuilder str = new StringBuilder("{");
         for (int i = 0; i < array.length; i++) {
             if (i > 0) {
-                str += ",";
+                str.append(",");
             }
-            str += "{";
+            str.append("{");
             for (int j = 0; j < array[0].length - 1; j++) {
-                str += array[i][j] + ", ";
+                str.append(array[i][j]).append(", ");
             }
-            str += array[i][array[0].length - 1] + "}";
+            str.append(array[i][array[0].length - 1]).append("}");
         }
-        str += "}";
-        return str;
+        str.append("}");
+        return str.toString();
     }
 
-    public Matrix getTranspositionMatrix() {
+    public Matrix getTransposition() {
         int y = array.length;
         int x = array[0].length;
         double[][] arrayTrp = new double[x][y];
@@ -94,8 +84,77 @@ public class Matrix {
     }
 
     public Vector getVectorColumn(int i) {
-        Matrix m = getTranspositionMatrix();
+        Matrix m = getTransposition();
         return m.getVector(i);
+    }
+
+    public void multiplicationByScalar(int s) {
+        int y = array.length;
+        int x = array[0].length;
+        for (int j = 0; j < y; j++) {
+            for (int k = 0; k < x; k++) {
+                array[j][k] *= s;
+            }
+        }
+    }
+
+    public int determinant() {
+        int y = array.length;
+        int x = array[0].length;
+
+        for (int i = 0; i < x; i++) {
+            for (int j = 0; j < x; j++) {
+                System.out.printf("%8.2f", array[i][j]);
+
+            }
+            System.out.println();
+        }
+        System.out.println();
+        System.out.println();
+
+        if (array[0][0] == 0) {
+            int notZeroPos = 0;
+            for (int i = 1; i < y; i++) {
+                if (array[i][0] == 0) {
+                    continue;
+                }
+                notZeroPos = i;
+                break;
+            }
+
+            if (notZeroPos == 0) {
+                return 0;
+            } else {
+                for (int i = 0; i < x; i++) {
+                    array[0][i] += array[notZeroPos][i];
+                }
+            }
+
+            for (int i = 0; i < x; i++) {
+                for (int j = 0; j < x; j++) {
+                    System.out.printf("%8.2f", array[i][j]);
+
+                }
+                System.out.println();
+            }
+            System.out.println();
+            System.out.println();
+
+
+            for (int i = 1; i < y; i++) {
+                for (int j = 0; j < x; j++) {
+                    array[i][j] = array[i][j]+ array[0][j] * (-array[i][0] / array[0][0]);
+                }
+            }
+
+            for (int i = 0; i < x; i++) {
+                for (int j = 0; j < x; j++) {
+                    System.out.printf("%8.2f", array[i][j]);
+                }
+                System.out.println();
+            }
+        }
+        return 0;
     }
 }
 
