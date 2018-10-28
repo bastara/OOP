12a4f3aa72361a13a2646170e5c44287c37ac59f
bastara <kolobokuu@yandex.ru>
2018-10-28 19:43:52 +0700

    vector исправил все ошибки 28/10/18.

--- Vector/src/ru/academit/kolobov/vector/Vector/Vector.java
+++ Vector/src/ru/academit/kolobov/vector/Vector/Vector.java
@@ -3,71 +3,66 @@ package ru.academit.kolobov.vector.Vector;
 import java.util.Arrays;
 
 public class Vector {
-    private double[] array;
+    private double[] arrayOfCoordinates;
 
     public Vector(int n) {
         if (n <= 0) {
             throw new IllegalArgumentException("Размерность прстранства 0 или отрицательная");
         }
-        array = new double[n];
+        arrayOfCoordinates = new double[n];
     }
 
     public Vector(Vector vector) {
-        array = Arrays.copyOf(vector.array, vector.array.length);
+        arrayOfCoordinates = Arrays.copyOf(vector.arrayOfCoordinates, vector.arrayOfCoordinates.length);
     }
 
     public Vector(double[] a) {
         if (a.length == 0) {
             throw new IllegalArgumentException("Размерность прстранства 0 или отрицательная");
         }
-        array = Arrays.copyOf(a, a.length);
+        arrayOfCoordinates = Arrays.copyOf(a, a.length);
     }
 
     public Vector(int n, double[] a) {
         if (a.length == 0 || n == 0) {
             throw new IllegalArgumentException("Размерность прстранства 0 или отрицательная");
         }
-        array = Arrays.copyOf(a, n);
+        arrayOfCoordinates = Arrays.copyOf(a, n);
     }
 
     public static Vector sumStatic(Vector v1, Vector v2) {
-        v1.add(v2);
-        return v1;
+        Vector v = new Vector(v1);
+        v.add(v2);
+        return v;
     }
 
     public static Vector difStatic(Vector v1, Vector v2) {
-        v1.subtraction(v2);
-        return v1;
+        Vector v = new Vector(v1);
+        v.subtraction(v2);
+        return v;
     }
 
     public static double scalar(Vector v1, Vector v2) {
-        int nMin = Math.min(v1.array.length, v2.array.length);
+        int nMin = Math.min(v1.arrayOfCoordinates.length, v2.arrayOfCoordinates.length);
         double scalar = 0;
         for (int i = 0; i < nMin; i++) {
-            scalar += v1.array[i] * v2.array[i];
+            scalar += v1.arrayOfCoordinates[i] * v2.arrayOfCoordinates[i];
         }
         return scalar;
     }
 
     public int getSize() {
-        if (array == null) {
-            throw new NullPointerException("Массив 0 размерности");
-        }
-        return array.length;
+        return arrayOfCoordinates.length;
     }
 
     @Override
     public String toString() {
-        StringBuilder str = new StringBuilder("{");
-        for (int i = 0; i < array.length - 1; i++) {
-            str.append(array[i]).append(", ");
-        }
-        return str.toString() + array[array.length - 1] + "}";
+        return Arrays.toString(arrayOfCoordinates).replaceAll("\\[","{").replaceAll("]","}");
     }
 
     @Override
     public int hashCode() {
-        return Arrays.hashCode(array);
+        return Arrays.hashCode(arrayOfCoordinates);
     }
 
     @Override
@@ -81,73 +76,62 @@ public class Vector {
         }
 
         Vector p = (Vector) o;
-        if (!Arrays.equals(this.array, p.array)) {
-            return false;
-        }
-        return array.length == p.array.length;// я так понимаю что размерность массивов проверится ранее и данной проверке нет необходимости, но т.к. стоит в условии задачи то оставил.
+        return !Arrays.equals(this.arrayOfCoordinates, p.arrayOfCoordinates);
     }
 
     public void add(Vector v2) {
-        int nMax = Math.max(this.array.length, v2.array.length);
-        int nMin = Math.min(this.array.length, v2.array.length);
-        if (array.length > v2.array.length) {
-            for (int i = 0; i < nMin; i++) {
-                array[i] = array[i] + v2.array[i];
-            }
-        } else {
+        int nMax = Math.max(this.arrayOfCoordinates.length, v2.arrayOfCoordinates.length);
+        int nMin = Math.min(this.arrayOfCoordinates.length, v2.arrayOfCoordinates.length);
+        for (int i = 0; i < nMin; i++) {
+            arrayOfCoordinates[i] += v2.arrayOfCoordinates[i];
+        }
+        if (arrayOfCoordinates.length < v2.arrayOfCoordinates.length) {
             double[] arr = new double[nMax];
-            for (int i = 0; i < nMin; i++) {
-                arr[i] = array[i] + v2.array[i];
-            }
-            System.arraycopy(v2.array, nMin, arr, nMin, nMax - nMin);
-            array = arr;
+            System.arraycopy(arrayOfCoordinates, 0, arr, 0, nMin);
+            System.arraycopy(v2.arrayOfCoordinates, nMin, arr, nMin, nMax - nMin);
+            arrayOfCoordinates = arr;
         }
     }
 
+
     public void subtraction(Vector v2) {
-        int nMax = Math.max(this.array.length, v2.array.length);
-        int nMin = Math.min(this.array.length, v2.array.length);
-        if (array.length > v2.array.length) {
-            for (int i = 0; i < nMin; i++) {
-                array[i] = array[i] - v2.array[i];
-            }
-        } else {
+        int nMax = Math.max(this.arrayOfCoordinates.length, v2.arrayOfCoordinates.length);
+        int nMin = Math.min(this.arrayOfCoordinates.length, v2.arrayOfCoordinates.length);
+        if (arrayOfCoordinates.length < v2.arrayOfCoordinates.length) {
             double[] arr = new double[nMax];
-            for (int i = 0; i < nMin; i++) {
-                arr[i] = array[i] - v2.array[i];
-            }
-            for (int i = nMin; i < nMax; i++) {
-                arr[i] = -v2.array[i];
-            }
-            array = arr;
+            System.arraycopy(arrayOfCoordinates, 0, arr, 0, nMin);
+            arrayOfCoordinates = arr;
+        }
+        for (int i = 0; i < v2.arrayOfCoordinates.length; i++) {
+            arrayOfCoordinates[i] -= v2.arrayOfCoordinates[i];
         }
     }
 
     public void invert() {
-        for (int i = 0; i < this.array.length; i++) {
-            array[i] = (this.array[i] == 0) ? 0 : this.array[i] * (-1);
+        for (int i = 0; i < this.arrayOfCoordinates.length; i++) {
+            arrayOfCoordinates[i] = (this.arrayOfCoordinates[i] == 0) ? 0 : this.arrayOfCoordinates[i] * (-1);
         }
     }
 
     public double getLength() {
         double sum = 0;
-        for (double c : this.array) {
+        for (double c : this.arrayOfCoordinates) {
             sum += c * c;
         }
         return Math.sqrt(sum);
     }
 
     public double getElement(int index) {
-        return this.array[index];
+        return this.arrayOfCoordinates[index];
     }
 
     public void setElement(int index, double c) {
-        this.array[index] = c;
+        this.arrayOfCoordinates[index] = c;
     }
 
     public void multiplicationVectorByScalar(int i) {
-        for (int j = 0; j < array.length; j++) {
-            array[j] *= i;
+        for (int j = 0; j < arrayOfCoordinates.length; j++) {
+            arrayOfCoordinates[j] *= i;
         }
     }
 }
